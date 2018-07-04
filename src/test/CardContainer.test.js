ba93348d90f439f8953b6a996790c0efd7581719
Andrew James <ajames20@gmail.com>
2018-07-04 05:05:51 -0600

    Add test coverage for router

--- src/test/CardContainer.test.js
+++ src/test/CardContainer.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import CardContainer from '../components/CardContainer/CardContainer';
+import { NavLink } from 'react-router-dom';
 
 describe('CardContainer', () => {
   let wrapper;
@@ -8,6 +9,10 @@ describe('CardContainer', () => {
   beforeEach(() => wrapper = shallow(<CardContainer />));
 
   afterEach(() => wrapper.unmount());
+
+  test('should render correct page based on route', () => {
+    expect(wrapper.find(NavLink).first().props().to).toEqual("/people");
+  });
 
   test('should match snapshot', () => {
     expect(wrapper).toMatchSnapshot();
--- src/test/Router.test.js
+++ src/test/Router.test.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { StaticRouter } from 'react-router';
+import Router from '../router/Router';
+import { Route } from 'react-router-dom';
 import App from '../components/App/App';
 
 describe('Router', () => {
   let wrapper;
 
   beforeEach(() => wrapper = shallow(
-    <StaticRouter context={{ path: '/' }}>
+    <Router>
       <App/>
-    </StaticRouter>
+    </Router>
   ));
 
   afterEach(() => wrapper.unmount());
+
+  test('should ', () => {
+    expect(wrapper.find(Route).props().path).toEqual('/');
+  });
 
   test('should not redirect to 404', () => {
     expect(wrapper.find(App)).toHaveLength(1);
